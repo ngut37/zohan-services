f7f6bb0dab3e393cde43f620195bbcd8afb5b8d3
Denny Nguyen <55693964+ngut37@users.noreply.github.com>
2022-09-21 16:23:08 +0200

    feat: 🎸 include staff creation in company registration

---
+++ src/controllers/companies/types.ts
@@ -0,0 +1,11 @@
+export type CompanyFormData = {
+  ico: string;
+  name: string;
+  // address: AddressInfo;
+  stringAddress: string;
+
+  regionString: string;
+  districtString: string;
+  /** momc */
+  quarterString?: string;
+};
