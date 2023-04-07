5fba0dfa6416566d169b53a9cef31bd45000ecb9
Denny Nguyen <55693964+ngut37@users.noreply.github.com>
2023-04-07 03:34:30 +0200

    feat: 🎸 add GET venue endpoint

--- src/controllers/venues/index.ts
+++ src/controllers/venues/index.ts
@@ -1,15 +1,17 @@
 import joiRouter from 'koa-joi-router';
 
 import createRouter from './create';
-import getAllRouter from './get-all';
 import editRouter from './edit';
+import getAllRouter from './get-all';
+import getRouter from './get';
 
 const router = joiRouter();
 
 router.prefix('/venues');
 
 router.use(createRouter.middleware());
-router.use(getAllRouter.middleware());
 router.use(editRouter.middleware());
+router.use(getAllRouter.middleware());
+router.use(getRouter.middleware());
 
 export default router;
