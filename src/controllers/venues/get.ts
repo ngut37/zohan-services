fc7e6c939c656fe7a747967e8394448e0fa21a40
Denny Nguyen <55693964+ngut37@users.noreply.github.com>
2023-04-07 03:34:30 +0200

    feat: 🎸 add GET venue endpoint

---
+++ src/controllers/venues/get.ts
@@ -0,0 +1,57 @@
+import joiRouter from 'koa-joi-router';
+
+import { adminProtectRouteMiddleware } from '@middlewares/admin-protect';
+import { Venue } from '@models/venue';
+
+import { CompanyAccessTokenPayload } from '@utils/company-auth';
+import { Region } from '@models/region';
+import { District } from '@models/district';
+import { Momc } from '@models/momc';
+
+const router = joiRouter();
+
+type RequestParams = {
+  id: string;
+};
+
+router.route({
+  path: '/:id',
+  method: 'get',
+  handler: [
+    adminProtectRouteMiddleware({
+      allowUnauthorized: false,
+    }),
+    async (ctx) => {
+      const { id } = ctx.request.params as RequestParams;
+      const { company } = ctx.state.auth as CompanyAccessTokenPayload;
+
+      const [foundVenue] = await Venue.find({
+        _id: id,
+        company,
+      }).populate<{
+        region: Region;
+        district: District;
+        momc?: Momc;
+      }>(['region', 'district', 'momc']);
+
+      if (!foundVenue) {
+        return ctx.throw(404, `Venue with ID "${id}" was not found.`);
+      }
+
+      const venuesWithPopulatedLocation = {
+        _id: foundVenue._id,
+        stringAddress: foundVenue.stringAddress,
+        region: foundVenue.region.name,
+        district: foundVenue.district.name,
+        momc: foundVenue.momc?.name,
+      };
+
+      ctx.body = {
+        success: true,
+        data: { ...venuesWithPopulatedLocation },
+      };
+    },
+  ],
+});
+
+export default router;
