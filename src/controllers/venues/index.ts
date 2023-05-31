7827fb80a6bb1618867b67ba348641b9fb69aa26
Denny Nguyen <55693964+ngut37@users.noreply.github.com>
2023-06-01 00:11:51 +0200

    feat: 🎸 implement venue detail API

--- src/controllers/venues/index.ts
+++ src/controllers/venues/index.ts
@@ -1,11 +1,13 @@
 import joiRouter from 'koa-joi-router';
 
 import listRouter from './list';
+import getRouter from './get-by-id';
 
 const router = joiRouter();
 
 router.prefix('/venues');
 
 router.use(listRouter.middleware());
+router.use(getRouter.middleware());
 
 export default router;
