04ccf81a81e2d378cf966fb3ed9fce60b75fa07b
Denny Nguyen <55693964+ngut37@users.noreply.github.com>
2024-05-11 00:45:44 +0200

    fix: 🐛 sanitize plus sign for emails

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,12 +1,9 @@
-export const periodsInEmailOptionalRegex = (email: string): RegExp => {
-  const matchDotsRegex = new RegExp(/\.(?=.*?@\w*\.\w*)/g);
-  const stringifiedRegex = email.replace(matchDotsRegex, '(.)?');
-
-  return new RegExp(stringifiedRegex, 'ig');
-};
-
 export const generateEmailRegex = (email: string): RegExp => {
-  const isGmail = /@gmail\.com/gi.test(email);
-  if (isGmail) return periodsInEmailOptionalRegex(email);
-  return new RegExp(`^${email}$`, 'ig');
+  const sanitizedEmail = email.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
+  const optionalCharactersEmail = sanitizedEmail
+    .replace(/\\\.(?=.*?@)/g, '\\.?')
+    .replace(/\\\+(?=.*?@)/g, '\\+?');
+
+  return new RegExp(`^${optionalCharactersEmail}$`, 'ig');
 };
